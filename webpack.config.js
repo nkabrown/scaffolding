5032ea2de81eace1050d04959091d0918873f72c
Nathan Brown <nkabrown@gmail.com>
2016-03-31 23:08:32 -0400

    Add linter

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,13 @@ module.exports = {
     path: '../dist'
   },
   devtools: debug ? 'sourcemaps' : null,
+  eslint: {
+    configFile: '.eslintrc.json'
+  },
   module: {
+    preLoaders: [
+      { test: /\.js?$/, exclude: /(node_modules|bower_components)/, loader: 'eslint' }
+    ],
     loader: [
       { test: /\.js?$/, exclude: /(node_modules|bower_components)/, loader: 'babel', query: { presets: ['es2015', 'stage-1'] } },
       { test: /\.css?$/, loader: 'style!css' }
